31777a17594f6e4d8e5f2b1b1d5f810e78506c9e
Erik Asensio <erikasensioperez@gmail.com>
2022-09-08 10:13:05 +0200

    adjustments

--- staff/erik-asensio/final-project/app/src/App.jsx
+++ staff/erik-asensio/final-project/app/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css"
 import LoginPage from "./pages/loginPage/loginPage"
 import HomePage from "./pages/homePage/homePage"
 import CreatePage from "./pages/createPage/createPage"
+import EditPage from "./pages/editPage/editPage"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/" element={<HomePage />} />
                 <Route path="/createAquo" element={<CreatePage />} />
+                <Route path="/editAquo" element={<EditPage />} />
             </Routes>
         </Router>
     </div>
--- staff/erik-asensio/final-project/app/src/pages/editPage/editPage.jsx
+++ staff/erik-asensio/final-project/app/src/pages/editPage/editPage.jsx
@@ -1,7 +1,58 @@
 import "./editPage.css"
+import backIcon from "../../img/createPage/backIcon.svg"
+import mainImg from "../../img/createPage/mainImage.png"
+import editIcon from "../../img/createPage/editIcon.svg"
+import { Link } from "react-router-dom"
 
-function EditPage(){
-    return <h1>EDIT PAGE</h1>
+function EditPage() {
+    return <div className="editPage">
+        <div className="editPage-header">
+            <Link to="/aquoPage"><img src={backIcon} alt="back" className="editPage-header--backIcon" /></Link>
+            <h1 className="editPage-header--title">Edit Aquo</h1>
+        </div>
+        <div className="editPage-img">
+            <img src={mainImg} alt="" className="editPage-img--mainImg" />
+            <img src={editIcon} alt="" className="editPage-img--editIcon" />
+        </div>
+        <div className="editPage-panel">
+            <div className="editPage-panel--form">
+                <div className="editPage-form--name">
+                    <label htmlFor="name" className="editPage-form--nameLabel editPage-form--label">Name:</label>
+                    <input type="text" id="name" className="editPage-form--nameInput editPage-form--input" />
+                </div>
+
+                <div className="editPage-form--type">
+                    <label htmlFor="type" className="editPage-form--typeLabel editPage-form--label">Type:</label>
+                    <input type="text" id="type" className="editPage-form--typeInput editPage-form--input" />
+                </div>
+
+                <div className="editPage-form--standards">
+                    <h3 className="editPage-form--standardTitle">Standard's:</h3>
+                    <p className="editPage-form--standardInfo">Set the standard values, if these values exceeds you will be notified</p>
+                </div>
+                <div className="editPage-form--standardInputs">
+
+                    <div className="editPage-form--input1">
+                        <label htmlFor="input1" className="editPage-form--input1Label editPage-form--standardLabel">input1:</label>
+                        <input type="text" id="input1" className="editPage-form--input1Input editPage-form--standardInput" />
+                    </div>
+
+                    <div className="editPage-form--input2">
+                        <label htmlFor="input2" className="editPage-form--input2Label editPage-form--standardLabel">input2:</label>
+                        <input type="text" id="input2" className="editPage-form--input2Input editPage-form--standardInput" />
+                    </div>
+
+                    <div className="editPage-form--input3">
+                        <label htmlFor="input3" className="editPage-form--input3Label editPage-form--standardLabel">input3:</label>
+                        <input type="text" id="input3" className="editPage-form--input3Input editPage-form--standardInput" />
+                    </div>
+                </div>
+                <div className="editPage-form--submit">
+                    <button className="editPage-form--submitButton">Update</button>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default EditPage
